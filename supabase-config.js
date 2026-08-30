c675a25fccb0dd2749324394f93db94ced96fcfa
jaeone811 <cocojjone9@gmail.com>
2026-08-31 02:01:30 +0900

    Connect Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
  * anon 키는 브라우저에 공개되어도 되는 키입니다. service_role 키는 절대 넣지 마세요.
  */
 window.POLITIMON_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://ngninlrunpfrahkboblj.supabase.co",
+  anonKey: "sb_publishable_tnPEieCk7YJH3emHc9cy7w_PZ_JPEJx"
 };
